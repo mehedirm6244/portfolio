3b34d23ff26802dc5bcfeee46c2d5ff5595d373b
Mehedi Rahman Mahi <mehedirm6244@protonmail.com>
2024-08-26 18:03:51 +0600

    Add notfound page

---
+++ _app/immutable/entry/start.9htV6ugy.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DwArdRRx.js";export{t as start};
---
+++ _app/immutable/nodes/4.CkSmI96B.js
@@ -0,0 +1,1 @@
+import{s}from"../chunks/scheduler.DUkfrwkU.js";import{S as t,i as e}from"../chunks/index.C5-k1b_n.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
